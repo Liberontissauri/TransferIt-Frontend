0122921294a8c7f3bdf1e60fddfd03c61e862d06
João Liberato <vaz.liberato@gmail.com>
2023-07-01 00:48:33 +0100

    added upload page

---
+++ src/Pages/Upload/Upload.tsx
@@ -0,0 +1,30 @@
+import { useUser } from "@/hooks/auth"
+import { useState } from "react"
+import styles from "./Upload.module.css"
+import Navbar from '@/components/Navbar/Navbar'
+import axios from "axios"
+
+const axios_instance = axios.create()
+
+function Upload() {
+    const user = useUser({
+        axios_instance: axios_instance,
+        post_logout_url: "/",
+        login_url: "/login"
+    })
+    const [FileName, setFileName] = useState("")
+    return (
+        <div className={styles.container}>
+            <Navbar></Navbar>
+            <div className={styles.formContainer} onClick={() => document.getElementById("file")?.click()}>
+                <form className={styles.uploadBox} id="upload_form" method="post" action="/api/files/upload" encType="multipart/form-data">
+                    <input className={styles.fileInput} type="file" id="file" name="file" onChange={e => {e.target.files && setFileName(e.target.files[0].name)}}></input>
+                    <p className={FileName ? styles.selectedFile : styles.noFile}>{FileName ? FileName : "Drop your file here..."}</p>
+                </form>
+            </div>
+            <button className={styles.submitUpload} form="upload_form" type="submit">Upload File</button>
+        </div>
+    )
+}
+
+export default Upload
--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,14 @@ import './index.css'
 import Home from './Pages/Home/Home';
 import Profile from './Pages/Profile/Profile';
 import Login from './Pages/Login/Login';
+import Upload from './Pages/Upload/Upload';
 
 const router = createBrowserRouter([
   { path: "/", element: <Hero /> },
   { path: "/dashboard", element: <Home></Home>},
   { path: "/profile", element: <Profile></Profile>},
-  { path: "/login", element: <Login></Login>}
+  { path: "/login", element: <Login></Login>},
+  { path: "/upload", element: <Upload></Upload>}
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
